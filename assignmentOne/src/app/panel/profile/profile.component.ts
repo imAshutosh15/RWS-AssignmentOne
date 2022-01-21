bdfc43b08e5c35f21cb813e53df2ffa27fa25df2
imAshutosh15 <96290026+imAshutosh15@users.noreply.github.com>
2022-01-21 13:54:36 +0530

    self profile update

--- assignmentOne/src/app/panel/profile/profile.component.ts
+++ assignmentOne/src/app/panel/profile/profile.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit , ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ToasterService } from '../../services/toaster/toaster.service';
 import { CommonService } from '../../services/common/common.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,24 +12,62 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ProfileComponent implements OnInit {
 
-  userData : any;
+  userData: any;
+  userid: any;
+  constructor(private modalService: NgbModal, private spinnerService: NgxSpinnerService, private toasterService: ToasterService, private commonService: CommonService) { }
 
-  constructor(private modalService: NgbModal,private spinnerService: NgxSpinnerService, private toasterService: ToasterService, private commonService: CommonService) { }
+  userProfileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+  });
 
   ngOnInit(): void {
     this.spinnerService.show();
     this.commonService.userProfile(localStorage.getItem("id")).subscribe((result: any) => {
       this.spinnerService.hide();
-      if (result.status == true) {        
+      if (result.status == true) {
         this.userData = result.items;
-      }else{
+      } else {
         this.toasterService.showError(result["message"], "Sorry!")
       }
     })
   }
 
-  open(content:any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  get userProfileControll() {
+    return this.userProfileForm.controls;
+  }
+
+  onSubmitProfileForm() {
+    this.userid = localStorage.getItem('id')
+    this.spinnerService.show();
+    this.commonService.updateProfile(this.userProfileForm.value, this.userid).subscribe((result: any) => {
+
+      this.spinnerService.hide();
+
+      if (result.subCode == 200) {
+        this.toasterService.showSuccess(result.message, "Profile Updated Successfully")
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+
+      } else {
+        this.toasterService.showError(result.message, "Sorry!")
+        setTimeout(() => {
+          window.location.reload();
+        }, 5000);
+      }
+    })
+  }
+
+  open(content: any, data: any) {
+    this.userData = data;
+    this.userProfileForm = new FormGroup({
+      firstName: new FormControl(this.userData.firstName, Validators.required),
+      lastName: new FormControl(this.userData.lastName, Validators.required),
+      gender: new FormControl(this.userData.gender, Validators.required),
+    });
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
   }
 
 }
